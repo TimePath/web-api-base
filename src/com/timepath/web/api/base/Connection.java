e6cddb4794ea48adf6f52ff3f0ed89a0c77579a4
TimePath <andrew.hardaker1995@gmail.com>
2013-10-18 20:44:27 +1100

    Cleaned up Connection code

--- src/com/timepath/web/api/base/Connection.java
+++ src/com/timepath/web/api/base/Connection.java
@@ -14,38 +14,22 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author timepath
+ * @author TimePath
  */
 public abstract class Connection {
 
-    private HttpURLConnection con;
-
-    URL url;
-
-    String base;
-
-    String method;
-
-    public Connection(String base, String method) throws MalformedURLException {
-        if(base != null) {
-            this.base = base;
-        }
-        this.method = method;
-        this.url = URI.create(getBaseUrl() + method).toURL();
-    }
-
-    public Connection(String method) throws MalformedURLException {
-        this(null, method);
-    }
-
     private static final Logger LOG = Logger.getLogger(Connection.class.getName());
+
+    protected String base;
 
     public String getBaseUrl() {
         return base;
     }
 
-    public void connect(String method) {
-        String address = "";
+    protected abstract long mindelay();
+
+    public HttpURLConnection connect(String method) throws MalformedURLException {
+        String address = getBaseUrl();
         if(method != null) {
             if(!address.endsWith("/") && !method.startsWith("/")) {
                 address += "/";
@@ -53,73 +37,57 @@ public abstract class Connection {
             address += method;
         }
         LOG.log(Level.INFO, "Connecting: {0}", address);
+        URL url = URI.create(address).toURL();
+        HttpURLConnection con = null;
         try {
             con = (HttpURLConnection) url.openConnection();
             con.setRequestProperty("User-Agent", getUserAgent());
             con.setReadTimeout(30000);
             con.setDoOutput(true);
-            onConnect();
+            onConnect(con);
         } catch(IOException ex) {
             LOG.log(Level.SEVERE, null, ex);
         }
+        connection = con;
+        return con;
     }
 
     public abstract String getUserAgent();
 
-    public HttpURLConnection getCon() {
-        return con;
+    protected void onConnect(HttpURLConnection con) {
     }
 
-    protected void onConnect() {
+    public String get(String method, boolean useCache) throws MalformedURLException {
+        HttpURLConnection con = connect(method);
+        return getm(con, useCache);
     }
 
-    ;
-
-    protected String postm(String data, boolean get) {
-        connect("");
-        System.out.println(">>> " + data);
-        try {
-            PrintWriter pw = new PrintWriter(new OutputStreamWriter(con.getOutputStream()));
-            pw.write(data);
-            pw.close();
-            if(get) {
-                return get(false);
-            }
-        } catch(IOException e) {
-            LOG.log(Level.SEVERE, "Unable to write: {0}", e.toString());
-        }
-        return null;
-    }
-
-    public String postget(String data) {
-        return postm(data, true);
-    }
-
-    public void post(String data) {
-        postm(data, false);
-    }
-
-    public String get() {
-        return get("");
-    }
-
-    public String get(boolean useCache) {
-        return get("", useCache);
-    }
-
-    public String get(String method) {
+    public String get(String method) throws MalformedURLException {
         return get(method, true);
     }
 
-    public String get(String method, boolean useCache) {
+    public void post(String method, String data) throws MalformedURLException {
+        postm(method, data, false);
+    }
+
+    public String postget(String method, String data) throws MalformedURLException {
+        return postm(method, data, true);
+    }
+
+    private HttpURLConnection connection;
+
+    public HttpURLConnection getCon() {
+        return connection;
+    }
+
+    public String getm(HttpURLConnection con, boolean useCache) {
         if(useCache) {
             String ret = getCache();
             if(ret != null) {
-                System.out.println("<<< " + ret);
+                LOG.log(Level.INFO, "<<< (cache) {0}", ret);
                 return ret;
             }
         }
-        connect(method);
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
             StringBuilder sb = new StringBuilder(8192);
@@ -133,18 +101,41 @@ public abstract class Connection {
                 Cache.write("", sb.toString());
             }
 
-            try {
-                Thread.sleep(2000L);
-            } catch(InterruptedException ex) {
-                LOG.log(Level.SEVERE, null, ex);
-            }
-            System.out.println("<<< " + sb.toString());
+//            if(mindelay() > 0) {
+//                try {
+//                    Thread.sleep(mindelay());
+//                } catch(InterruptedException ex) {
+//                    LOG.log(Level.SEVERE, null, ex);
+//                }
+//            }
+            LOG.log(Level.INFO, "<<< {0}", sb.toString());
             return sb.toString();
         } catch(IOException e) {
             e.printStackTrace();
             LOG.log(Level.WARNING, "READ FAILED: {0}", e.toString());
             return null;
         }
+    }
+
+    protected String postm(String method, String data, boolean get) throws MalformedURLException {
+        HttpURLConnection con = connect(method);
+        LOG.log(Level.INFO, ">>> {0}", data);
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(new OutputStreamWriter(con.getOutputStream()));
+            pw.write(data);
+            pw.flush();
+        } catch(IOException e) {
+            LOG.log(Level.SEVERE, "Unable to write: {0}", e.toString());
+        } finally {
+            if(pw != null) {
+                pw.close();
+            }
+        }
+        if(get) {
+            return getm(con, false);
+        }
+        return null;
     }
 
     private String getCache() {
